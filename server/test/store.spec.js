078027456caa4b0a642bd370c299c67422c30ea1
Patrik <p.thorsson@gmail.com>
2018-07-18 12:32:45 +0200

    Added tests for store message methods

--- server/test/store.spec.js
+++ server/test/store.spec.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const chai = require('chai');
 const expect = chai.expect;
 const chaiSubset = require('chai-subset');
@@ -53,6 +54,7 @@ it('Get employees', done => {
     expect(store.employee.get('Barry+Allen').code, 'Get employee by name 1').to.equals(200);
     expect(store.employee.get('O\'Barry+von+Allen').code, 'Get employee by name 2').to.equals(200);
     expect(store.employee.get('Bärrü+Ällëñ').code, 'Get employee by name 3').to.equals(200);
+    expect(store.employee.get('thisisnotanid').code, 'No employee found by id').to.equals(404);
 
     done();
 });
@@ -116,8 +118,165 @@ it('Remove employee', done => {
     let results;
 
     results = store.employee.remove('Bärrü+Ällëñ');
-    expect(results.code, 'Get employee by name').to.equals(200);
+    expect(results, 'Remove employee by name').to.containSubset({
+        code: 200,
+        data: 'OK'
+    });
     expect(store.employee.getAll().data.length, 'Get employee by name').to.equals(2);
     
+    done();
+});
+
+//
+//  Message
+//
+
+it('Add messages', done => {
+    let results;
+
+    globals.eid1 = store.employee.add({ firstName: 'Oliver', lastName: 'Queen' }).data;
+    globals.eid2 = store.employee.add({ firstName: 'Kara', lastName: 'Danvers' }).data;
+
+    // -- Create new messages
+
+    results = store.message.add({
+        employee: globals.eid1,
+        message: 'Message 1 by Oliver',
+        expiresAt: moment().add(10, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 1').to.containSubset({
+        code: 200,
+        data: id => uuidPattern.test(id)
+    });
+
+    results = store.message.add({
+        employee: globals.eid1,
+        message: 'Message 2 by Oliver',
+        expiresAt: moment().add(1, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 2').to.containSubset({
+        code: 200,
+        data: id => uuidPattern.test(id)
+    });
+
+    results = store.message.add({
+        employee: globals.eid2,
+        message: 'Message 1 by Kara',
+        expiresAt: moment().add(1, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 1 by kara').to.containSubset({
+        code: 200,
+        data: id => uuidPattern.test(id)
+    });
+
+    results = store.message.add({
+        employee: globals.eid2,
+        message: 'Message 2 by Kara',
+        expiresAt: moment().add(12, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 2 by kara').to.containSubset({
+        code: 200,
+        data: id => uuidPattern.test(id)
+    });
+
+    // -- Fails to create new messages
+
+    results = store.message.add({
+        employee: globals.eid1,
+        message: 'Message 3 by Oliver',
+        expiresAt: moment().add(0, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 3 with invalid date').to.containSubset({
+        code: 400,
+        data: 'Invalid expiration date'
+    });
+
+    results = store.message.add({
+        employee: globals.eid1,
+        message: 'Message 4 by Oliver',
+        expiresAt: moment().add(-10, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 4 with invalid date').to.containSubset({
+        code: 400,
+        data: 'Invalid expiration date'
+    });
+
+    results = store.message.add({
+        employee: 'thisisnotanid',
+        message: 'Message 5 by Oliver',
+        expiresAt: moment().add(1, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Add message 5 with invalid employee').to.containSubset({
+        code: 400,
+        data: 'No employee with the given id'
+    });
+    
+    done();
+});
+
+it('Get messages', done => {
+    let results;
+
+    globals.mid1 = store.message.add({
+        employee: globals.eid1,
+        message: 'Test message',
+        expiresAt: moment().add(10, 'days').format('YYYY-MM-DD')
+    }).data;
+    expect(store.message.get(globals.mid1).code, 'Get message by id').to.equals(200);
+    expect(store.message.get('thisisnotanid').code, 'No message found by id').to.equals(404);
+
+    done();
+});
+
+it('Get batched messages', done => {
+    let results;
+
+    results = store.message.getBatched();
+    expect(results.code, 'Get batched messages response code').to.equals(200);
+    expect(results.data.length, 'Employee count').to.equals(2);
+    expect(results.data[0].messages.length, 'Employee 1 message count').to.equals(3);
+    expect(results.data[1].messages.length, 'Employee 2 message count').to.equals(2);
+
+    done();
+});
+
+it('Get and update a message', done => {
+    let results;
+
+    results = store.message.update(globals.mid1, {
+        employee: globals.eid1,
+        message: 'Test message',
+        expiresAt: moment().add(15, 'days').format('YYYY-MM-DD')
+    });
+    expect(results, 'Updates message by id').to.containSubset({
+        code: 200,
+        data: 'OK'
+    });
+
+    results = store.message.get(globals.mid1);
+    expect(results, 'Confirms new message data').to.containSubset({
+        code: 200,
+        data: {
+            message: 'Test message',
+            expiresAt: date => date === moment().add(15, 'days').format('YYYY-MM-DD')
+        }
+    });
+
+    done();
+});
+
+it('Remove one and get all message', done => {
+    let results;
+
+    expect(store.message.getAll().data.length, 'Get all messages before remove').to.equals(5);
+
+    results = store.message.remove(globals.mid1);
+    expect(results, 'Remove message by id').to.containSubset({
+        code: 200,
+        data: 'OK'
+    });
+
+    expect(store.message.getAll().data.length, 'Get all messages after remove').to.equals(4);
+
     done();
 });
