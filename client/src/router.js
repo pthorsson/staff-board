896f248504c35000cd9a550adccd6ee3031d8fb2
Fredrik <frekvent@gmail.com>
2018-08-21 13:38:43 +0200

    added board display vue
    added board display, to serve as main presentation screen

--- client/src/router.js
+++ client/src/router.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Employees from './views/Employees.vue'
+import BoardDisplay from './views/BoardDisplay.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueSocketio from 'vue-socket.io'
 
 Vue.use(Router)
 Vue.use(VueAxios, axios)
+Vue.use(VueSocketio, 'http://localhost:9000')
 
 export default new Router({
   mode: 'history',
@@ -20,6 +23,11 @@ export default new Router({
       path: '/employees',
       name: 'employees',
       component: Employees
+    },
+    {
+      path: '/boarddisplay',
+      name: 'boarddisplay',
+      component: BoardDisplay
     }
   ]
 })
---
+++ client/src/views/BoardDisplay.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+    <div>
+      <div class="grid-container">
+        <table class="mt-20">
+          <tr>
+            <th>Name</th>
+            <th>Message and expire date</th>
+          </tr>
+          <tr v-for="employee in messageBatch" v-bind:key="employee.id">
+            <td>{{employee.firstName}} {{employee.lastName}}</td>
+            <td>
+              <ul>
+                <li v-for="message in employee.messages" v-bind:key="message.id">
+                  {{message.message}} - Expires: {{message.expiresAt}}
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+const baseURL = 'http://localhost:9000'
+
+export default {
+  name: 'boardDisplay',
+  data: function () {
+    return {
+      messageBatch: null,
+      socketConnected: false
+    }
+  },
+  methods: {
+    getBoard: function () {
+      const url = baseURL + '/api/messages/batched'
+      this.axios.get(url).then((response) => {
+        this.messageBatch = response.data
+        console.log('Messages fetched')
+      })
+    }
+  },
+  sockets: {
+    connect (data) {
+      this.socketConnected = true
+      this.messageBatch = data
+      console.log('socket connected')
+    },
+    disconnect () {
+      this.socketConnected = false
+      console.log('socket disconnected')
+    },
+    state (data) {
+      this.messageBatch = data
+    }
+  },
+  created: function () {
+    // when component is created we fetch the data needed to view the board
+    // this.getBoard()
+  }
+}
+</script>
